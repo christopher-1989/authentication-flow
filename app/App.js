f0b1cb7ac23b2b5ee9c80c42e3bafa5daa80bacf
Christopher McDonald <c.robin.mcdonald@gmail.com>
2021-07-24 11:27:00 +1000

    Refactored AppNavigator to its own file. Next steps include refactoring toggleLoadingStatus to be part of reducers

--- app/App.js
+++ app/App.js
@@ -1,76 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { Provider, useSelector, useDispatch } from 'react-redux';
-import { CreateAccount } from '../screens/CreateAccountScreen';
-import { SignIn } from '../screens/SignInScreen';
+import { Provider } from 'react-redux';
 import { store } from './store';
-import { isSigningOut, isLoggedIn } from '../features/UserSlice';
-import { isLoading } from '../features/LoadingSlice';
-import { ProfileScreen } from '../screens/ProfileScreen';
-import { SettingsScreen } from '../screens/SettingsScreen';
-import { SplashScreen } from '../screens/SplashScreen';
-import { HomeScreen } from '../screens/HomeScreen';
-import { SecureStore } from 'expo-secure-store';
-import { RESTORE_TOKEN } from '../features/UserSlice';
-
-
-const Stack = createStackNavigator();
-export const AppNavigator = () => {
-  const dispatch = useDispatch();
-
-  
-  useEffect(() => {
-    const bootstrapAsync = async () => {
-      let userToken;
-
-      try {
-        console.log('trying to restore token from securestore');
-        userToken = await SecureStore.getItemAsync('userToken');
-      } catch (e) {
-        // Restoring token failed
-        console.log('restoring token failed')
-      }
-      // After restoring token, we may need to validate it in production apps
-      // This will switch to the App screen or Auth screen and this loading
-      // screen will be unmounted and thrown away.
-      console.log(`setting token: ${userToken}`); //If userToken is undefined, then the create account and login pages will be shown to the user
-      dispatch(RESTORE_TOKEN({token: userToken }));
-    };
-
-    bootstrapAsync();
-  }, []);
-
-  const loginStatus = useSelector(isLoggedIn);
-  const loadingStatus = useSelector(isLoading);
-  const signOutStatus = useSelector(isSigningOut);
-
-  if (loadingStatus) {
-    return <SplashScreen />;
-  }
-
-  return (
-    <Stack.Navigator>
-      {loginStatus ? (
-        <>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Profile" component={ProfileScreen} />
-          <Stack.Screen name="Settings" component={SettingsScreen} />
-        </>
-      ) : (
-        <>
-          <Stack.Screen name="CreateAccount" component={CreateAccount} />
-          <Stack.Screen name="Login" component={SignIn} options={{
-            title: 'Sign in',
-            // When logging out, a pop animation feels intuitive
-            // You can remove this if you want the default 'push' animation
-            animationTypeForReplace: signOutStatus ? 'pop' : 'push',
-          }} />
-        </>
-      )}      
-    </Stack.Navigator>
-  );
-};
+import { AppNavigator } from './AppNavigator';
 
 const App = () => {
   
